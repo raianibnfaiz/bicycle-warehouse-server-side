c0971f61ce98dc2d451bec486501f66761d8a8a1
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2023-07-20 23:03:57 +0600

    initializing app

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    // if (!authHeader) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    // }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        // if (err) {
+        //     return res.status(403).send({ message: 'Forbidden Access' })
+        // }
+        console.log('decoded', decoded)
+        req.decoded = decoded;
+    })
+    console.log('inside verifyJWT', authHeader);
+    next();
+}
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuj6pix.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('warehouse').collection('bicycle');
+
+        //auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ accessToken });
+        })
+
+        // services api
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.get('/myProducts', async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+
+            const query = { email: email };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+            // else {
+            //     res.status(403).send({ message: 'forbidden access' })
+            // }
+
+        })
+
+
+
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updatedDoc = {
+                $set: {
+
+                    quantity: updatedProduct.quantity,
+
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(updatedProduct);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("Warehouse server is running successfully");
+})
+
+app.listen(port, () => {
+    console.log("listening to server")
+})
